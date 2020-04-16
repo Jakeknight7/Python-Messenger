7c4ec9eb9ab4f18cd663f3421513edb7e1a843d6
Jakeknight7 <36526882+Jakeknight7@users.noreply.github.com>
2020-04-15 21:11:19 -0400

    Update socket_connection.py

--- socket_connection.py
+++ socket_connection.py
@@ -16,6 +16,6 @@ class Connection(Borg):
         if self._shared_state:
             return
         self.HOST = '127.0.0.1'
-        self.PORT = 61831
+        self.PORT = 12345
         self.socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket_connection.connect((self.HOST, self.PORT))
